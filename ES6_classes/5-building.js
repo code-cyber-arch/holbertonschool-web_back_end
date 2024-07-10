61691cafb02ac4307509af78850337458bfdf7a3
code-cyber-arch <archabdulrm@gmail.com>
2024-07-10 19:28:56 +1000

    5-building.js

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,18 +1,14 @@
 export default class Building {
-  constructor(sqft) {
-    if (this.target === Building) {
-      throw new TypeError('Cannot construct Building instances directly');
+  constructor(sqft = 0) {
+    if (this.constructor !== Building
+        && typeof this.evacuationWarningMessage !== 'function') {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
     }
+
     this._sqft = sqft;
   }
 
   get sqft() {
     return this._sqft;
-  }
-
-  evacuationWarningMessage() {
-    if (this) {
-      throw new Error('Class extending Building must override evacuationWarningMessage');
-    }
   }
 }
