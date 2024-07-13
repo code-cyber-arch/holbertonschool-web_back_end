8df84b6c025f442dc0bfd6de7cfaf9e5faad2f73
code-cyber-arch <archabdulrm@gmail.com>
2024-07-13 10:27:18 +1000

    Add get and set

--- ES6_classes/7-airport.js
+++ ES6_classes/7-airport.js
@@ -4,7 +4,31 @@ export default class Airport {
     this._code = typeof code === 'string' ? name : '';
   }
 
-  toSrting() {
+  get [Symbol.toSrting]() {
+    return `${this._code}`;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(value) {
+    if (typeof value === 'string') {
+      this._name = value;
+    } else {
+      throw new TypeError('Name must be a string');
+    }
+  }
+
+  get code() {
     return this._code;
+  }
+
+  set code(value) {
+    if (typeof value === 'string') {
+      this._code = value;
+    } else {
+      throw new TypeError('Code must be a string');
+    }
   }
 }
