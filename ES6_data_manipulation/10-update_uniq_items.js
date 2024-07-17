74916819cfb9d067d1b49d94e14b1fdff7a5d4e7
code-cyber-arch <archabdulrm@gmail.com>
2024-07-17 14:31:02 +1000

    10-update_uniq_items.js

--- ES6_data_manipulation/10-update_uniq_items.js
+++ ES6_data_manipulation/10-update_uniq_items.js
@@ -2,13 +2,12 @@ export default function updateUniqueItems(groceriesMap) {
   if (!(groceriesMap instanceof Map)) {
     throw new Error('Cannot process');
   }
-  const updatedMAp = new Map();
   groceriesMap.forEach((qauntity, item) => {
     if (qauntity === 1) {
-      updatedMAp.set(item, 100);
+      groceriesMap.set(item, 100);
     } else {
-      updatedMAp.set(item, qauntity);
+      groceriesMap.set(item, qauntity);
     }
   });
-  return updatedMAp;
+  return groceriesMap;
 }
