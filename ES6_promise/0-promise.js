deb8ed749dbe68498730973994421c648494b7b0
code-cyber-arch <archabdulrm@gmail.com>
2024-07-17 15:31:22 +1000

    0-promise.js

---
+++ ES6_promise/0-promise.js
@@ -0,0 +1,10 @@
+export default function getResponseFromAPI() {
+  return new Promise((resolve, reject) => {
+    /* eslint-disable */
+        if (true) {
+            resolve();
+        } else {
+            reject();
+        }
+    });
+}
