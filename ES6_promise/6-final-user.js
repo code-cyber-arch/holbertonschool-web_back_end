8c422d3cc5adb79d6477036b1d14fe233d267ba3
code-cyber-arch <archabdulrm@gmail.com>
2024-07-18 11:40:53 +1000

    fix 2

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -2,5 +2,17 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName = '', lastName = '', fileName = '') {
-  return Promise.allSettled([uploadPhoto(fileName), signUpUser(firstName, lastName)]);
+  const signUpPromise = signUpUser(firstName, lastName)
+    .then((value) => ({
+      status: 'fulfilled',
+      value,
+    }));
+
+  const uploadPhotoPromise = uploadPhoto(fileName)
+    .catch((message) => ({
+      status: 'rejected',
+      value: message.toString(),
+    }));
+
+  return Promise.all([signUpPromise, uploadPhotoPromise]);
 }
