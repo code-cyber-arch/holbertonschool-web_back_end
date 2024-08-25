1dbfd29ca78f174616570234b7aae9956b03442b
code-cyber-arch <archabdulrm@gmail.com>
2024-08-25 17:55:04 +1000

    8 fix

--- NoSQL/8-all.py
+++ NoSQL/8-all.py
@@ -1,10 +1,19 @@
 #!/usr/bin/env python3
-""" List all documents in Python """
+""" List documents """
 import pymongo
 
 
-def list_all(mongo_collection):
-    """ lists all documents in a collection """
-    if not mongo_collection:
-        return []
-    return list(mongo_collection.find())
+def list_all(mongo_collection) -> list:
+    """ Lists all documents in a collection
+        Args:
+            mongo_collection: Collection of object
+
+        Return:
+            List with documents, otherwise []
+    """
+    documents: list = []
+
+    for document in mongo_collection.find():
+        documents.append(document)
+
+    return documents
