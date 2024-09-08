750fcbf0d90d339299e6a0ab41ac20070a39a1ea
code-cyber-arch <archabdulrm@gmail.com>
2024-09-08 18:09:22 +1000

    6-http_express.js

---
+++ Node_JS_basic/6-http_express.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+const port = 1245;
+
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
+});
+
+app.listen(port, () => {
+  // Server is now running and listening for requests
+});
+
+module.exports = app;
